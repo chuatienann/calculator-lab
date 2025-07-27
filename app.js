335f934de806ac69e5575a215c17c60e3f1e3495
chuatienann <tienannchua@gmail.com>
2025-07-27 22:52:17 +0800

    second commit

--- app.js
+++ app.js
@@ -3,8 +3,48 @@ const buttons = document.querySelectorAll('.button');
 const calculator = document.querySelector('#calculator');
 
 /*-------------------------------- Variables --------------------------------*/
-
+let firstNumber = '';
+let secondNumber = '';
+let operator = '';
+let resultDisplayed = false;
 /*------------------------ Cached Element References ------------------------*/
+
+/*-------------------------------- Functions --------------------------------*/
+function updateDisplay(value) {
+	display.textContent = value;
+}
+
+function clearDisplay() {
+		firstNumber = '';
+		secondNumber = '';
+		operator = '';
+		resultDisplayed = false;
+		updateDisplay('');
+}
+
+function evaluate() {
+		if (firstNumber && operator && secondNumber) {
+			const num1 = parseFloat(firstNumber);
+			const num2 = parseFloat(secondNumber);
+			let result;
+
+			if (operator === '+') {
+				result = num1 + num2;
+			} else if (operator === '-') {
+				result = num1 - num2;
+			} else if (operator === '*') {
+				result = num1 * num2;
+			} else if (operator === '/') {
+				result = num1 / num2;
+			}
+
+			updateDisplay(result);
+			firstNumber = result.toString();
+			secondNumber = '';
+			operator = '';
+			resultDisplayed = true;
+		}
+}
 
 /*----------------------------- Event Listeners -----------------------------*/
 // buttons.forEach((button) => {
@@ -16,19 +56,39 @@ const calculator = document.querySelector('#calculator');
 // });
 
 calculator.addEventListener('click', (event) => {
-  // This log is for testing purposes to verify we're getting the correct value
-  // You have to click a button to see this log
-  console.log(event.target.innerText);
+	const clickedButton = event.target;
+	const buttonValue = clickedButton.innerText;
 
-  // Example
-  if (event.target.classList.contains('number')) {
-    // Do something with a number
-  }
+	if (!clickedButton.classList.contains('button')) return;
 
-  // Example
-  if (event.target.innerText === '*') {
-    // Do something with this operator
-  }
+	if (buttonValue === 'C') {
+    clearDisplay();
+		return;
+	}
+
+	if (buttonValue === '=') {
+    evaluate();
+		return;
+	}
+
+	if (buttonValue === '+' || buttonValue === '-' || buttonValue === '*' || buttonValue === '/') {
+		if (firstNumber) {
+			operator = buttonValue;
+			updateDisplay(firstNumber + ' ' + operator);
+		}
+		return;
+	}
+
+	if (!operator) {
+		if (resultDisplayed) {
+			firstNumber = '';
+			resultDisplayed = false;
+		}
+		firstNumber += buttonValue; // these are still strings
+		updateDisplay(firstNumber);
+	} else {
+		secondNumber += buttonValue; // these are still strings
+		updateDisplay(firstNumber + ' ' + operator + ' ' + secondNumber);
+	}
 });
 
-/*-------------------------------- Functions --------------------------------*/
