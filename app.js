df422733308a1f80045656a880b3ab634fed56c2
chuatienann <tienannchua@gmail.com>
2025-07-27 23:10:09 +0800

    submit

--- app.js
+++ app.js
@@ -1,94 +1,148 @@
 /*-------------------------------- Constants --------------------------------*/
+const valuesToCalculate = [];           
+const operatorsToCalculate = []; 
+/*-------------------------------- Variables --------------------------------*/
+let result = "";                      
+let displayString = "";                 
+let currentValue = ""; 
+/*------------------------ Cached Element References ------------------------*/
 const buttons = document.querySelectorAll('.button');
 const calculator = document.querySelector('#calculator');
-
-/*-------------------------------- Variables --------------------------------*/
-let firstNumber = '';
-let secondNumber = '';
-let operator = '';
-let resultDisplayed = false;
-/*------------------------ Cached Element References ------------------------*/
-
-/*-------------------------------- Functions --------------------------------*/
-function updateDisplay(value) {
-	display.textContent = value;
-}
-
-function clearDisplay() {
-		firstNumber = '';
-		secondNumber = '';
-		operator = '';
-		resultDisplayed = false;
-		updateDisplay('');
-}
-
-function evaluate() {
-		if (firstNumber && operator && secondNumber) {
-			const num1 = parseFloat(firstNumber);
-			const num2 = parseFloat(secondNumber);
-			let result;
-
-			if (operator === '+') {
-				result = num1 + num2;
-			} else if (operator === '-') {
-				result = num1 - num2;
-			} else if (operator === '*') {
-				result = num1 * num2;
-			} else if (operator === '/') {
-				result = num1 / num2;
-			}
-
-			updateDisplay(result);
-			firstNumber = result.toString();
-			secondNumber = '';
-			operator = '';
-			resultDisplayed = true;
-		}
-}
+const displayDiv = document.querySelector('.display');
 
 /*----------------------------- Event Listeners -----------------------------*/
-// buttons.forEach((button) => {
-//   button.addEventListener('click', (event) => {
-//     // This log is for testing purposes to verify we're getting the correct value
-//     console.log(event.target.innerText);
-//     // Future logic to capture the button's value would go here...
-//   });
-// });
+buttons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+    buttonText = event.target.innerText;
 
-calculator.addEventListener('click', (event) => {
-	const clickedButton = event.target;
-	const buttonValue = clickedButton.innerText;
+    switch(event.target.className) {
+        case "button number":
 
-	if (!clickedButton.classList.contains('button')) return;
+            if (result){                
+                result = null;          
+                displayString = "";    
+            } 
 
-	if (buttonValue === 'C') {
-    clearDisplay();
-		return;
-	}
+            if (currentValue !== null) {
+                currentValue = Number(currentValue.toString() + buttonText);
+            }       
+            
+            else {
+                currentValue = Number(buttonText);
+            }                          
 
-	if (buttonValue === '=') {
-    evaluate();
-		return;
-	}
+            displayString += buttonText; 
+            showDisplay();
+            break;
 
-	if (buttonValue === '+' || buttonValue === '-' || buttonValue === '*' || buttonValue === '/') {
-		if (firstNumber) {
-			operator = buttonValue;
-			updateDisplay(firstNumber + ' ' + operator);
-		}
-		return;
-	}
+        case "button equals":
+            finaliseCurrentValue();
+                if (valuesToCalculate.length !== operatorsToCalculate.length+1) {
+                    error();
+                    return;
+                } else {
+                    displayString = "";
+                    calculate();
+                    result = valuesToCalculate[0];
+                    currentValue = null;
+                    showResult();
+                    valuesToCalculate.length = 0;
+                    operatorsToCalculate.length = 0;
+                    return;
+                }
+            
+            break;
 
-	if (!operator) {
-		if (resultDisplayed) {
-			firstNumber = '';
-			resultDisplayed = false;
-		}
-		firstNumber += buttonValue; // these are still strings
-		updateDisplay(firstNumber);
-	} else {
-		secondNumber += buttonValue; // these are still strings
-		updateDisplay(firstNumber + ' ' + operator + ' ' + secondNumber);
-	}
+        case "button operator":
+
+            switch(buttonText) {
+                case "C":
+                    finaliseCurrentValue();
+                    clear();
+                    break;
+                
+                default: 
+                    finaliseCurrentValue();
+                    displayString += buttonText;
+                    showDisplay();
+                    operatorsToCalculate.push(buttonText);
+            }
+            break;
+    }
+  });
 });
+
+/*-------------------------------- Functions --------------------------------*/
+function showDisplay() {
+    displayDiv.textContent = displayString;
+}
+
+function clear() {
+    currentValue = null;
+    result = null;
+    displayString = "";
+    valuesToCalculate.length = 0;
+    operatorsToCalculate.length = 0;
+
+    displayDiv.textContent = "C";
+    setTimeout(() => {
+        displayDiv.textContent = "";
+    }, 1000);
+}
+
+
+function calculate() {
+
+    while (operatorsToCalculate.length !== 0) {
+
+        operatorsToCalculate.forEach((operator, index) => {
+            if (operator === "/") {
+                valuesToCalculate.splice(index, 2, valuesToCalculate[index]/valuesToCalculate[index+1]);
+                operatorsToCalculate.splice(index, 1);
+            } 
+            if (operator === "*") {
+                valuesToCalculate.splice(index, 2, valuesToCalculate[index]*valuesToCalculate[index+1]);
+                operatorsToCalculate.splice(index, 1);
+            } 
+                
+        });
+
+        if (!operatorsToCalculate.some((operator) => {return operator === "/" || operator === "*";})) {
+            operatorsToCalculate.forEach((operator, index) => {
+                if (operator === "+") {
+                    valuesToCalculate.splice(index, 2, valuesToCalculate[index]+valuesToCalculate[index+1]);
+                    operatorsToCalculate.splice(index, 1);
+                } 
+                if (operator === "-") {
+                    valuesToCalculate.splice(index, 2, valuesToCalculate[index]-valuesToCalculate[index+1]);
+                    operatorsToCalculate.splice(index, 1);
+                }
+            });
+        }
+    }
+}
+
+function finaliseCurrentValue() { 
+    if (result === null) {
+        if (currentValue !== null) {
+            valuesToCalculate.push(currentValue);
+            currentValue = null;
+        } else if (valuesToCalculate.length === 0) {
+            valuesToCalculate.push(0) 
+        }
+    } else {
+        valuesToCalculate.push(result);
+        result = null;
+    }
+}
+
+function showResult() {
+    displayDiv.textContent = result.toString();
+    displayString = result.toString();
+}
+
+function error() {
+    clear()
+    displayDiv.textContent = "error";
+}
 
